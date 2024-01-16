ba2ac4b174591dae6ecf46d805dfc05dd43c7f90
Isabela <isabela.leeite@hotmail.com>
2024-01-16 14:33:12 -0300

    Teste

---
+++ exercicio01.sql
@@ -0,0 +1,17 @@
+ /* Questão 1
+
+Você é responsável por controlar os dados de clientes e de produtos da
+sua empresa. O que você precisará fazer é confirmar se:
+
+a. Existem 2.517 produtos cadastrados na base e, se não tiver, você
+deverá reportar ao seu gestor para saber se existe alguma defasagem
+no controle dos produtos.
+
+b. Até o mês passado, a empresa tinha um total de 19.500 clientes na
+base de controle. Verifique se esse número aumentou ou reduziu. */
+
+SELECT * FROM DimProduct
+-- Existem 2.517 produtos cadastrados na base.
+
+SELECT * FROM DimCustomer
+-- Possui 19.145. Teve uma diminuição de 355 clientes.
---
+++ first-consult.sql
@@ -0,0 +1,9 @@
+SELECT * FROM DimCustomer
+
+SELECT * FROM DimStore
+
+SELECT StoreKey, StoreName, StorePhone FROM DimStore
+
+SELECT * FROM DimProduct
+
+SELECT ProductName, BrandName FROM DimProduct
